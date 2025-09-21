00bfbb9b2106d223c5656e20827c2e73f90994d5
Théo H. <thelava12@gmail.com>
2025-09-21 12:45:56 +0200

    push to save dont remember mb

--- lua/nux/nux.lua
+++ lua/nux/nux.lua
@@ -42,63 +42,20 @@ Nux.refresh = function()
 	if not H.is_active() then return end
 end
 
+-- TODO Open workspace_adder from the selection menu
+-- TODO Relative windows : hard coded atm
 -- TODO Once the root is selected, navigate through files to add them.
 -- TODO How to add a term ? Maybe it (S) to (S)pecial windows select term or processes and add some commands to run ? `:term jupyter notebook` will run only the notebook and will be discarded at the shutdown
 Nux.workspace_adder = function(on_confirm)
-	local buf = vim.api.nvim_create_buf(false, false)
-	vim.bo[buf].buftype = 'prompt'
-	-- TODO : completion function for folders
-	vim.bo[buf].bufhidden = 'wipe'
-	local show_config = H.get_window_config()
-	local shower = H.create_floating_window(show_config, false)
-
-	local prompt = "Testing prompt "
-
-	local defered_callback = function(input)
-		vim.defer_fn(function()
-			on_confirm(input)
-		end, 10)
-	end
-
-	vim.fn.prompt_setprompt(buf, '')
-	vim.fn.prompt_setcallback(buf, defered_callback)
-
-	vim.api.nvim_create_autocmd('TextChangedI', {
-		buffer = buf,
-		callback = function()
-			-- vim.print(vim.fn.prompt_getinput(buf))
-			local ns = vim.api.nvim_create_namespace("MyHighlightNs")
-			local user_path = H.parse_path(vim.fn.prompt_getinput(buf))
-			local files = vim.fn.readdir(user_path)
-			vim.api.nvim_buf_set_lines(shower.buf, 0, -1, false, files)
-			for i, v in pairs(files) do
-				local stat = vim.uv.fs_stat(user_path .. '/' .. v)
-				-- vim.api.nvim_buf_set_lines(shower.buf, i - 1, i - 1, false, { v })
-				local hl = (stat and stat.type == "directory") and "Directory" or "Normal"
-				vim.api.nvim_buf_set_extmark(shower.buf, ns, i - 1, 0, {
-					end_line = i,
-					hl_group = hl,
-				})
-			end
-			-- vim.api.nvim_buf_set_lines(shower.buf, 0, -1, false, files)
-		end
-	})
-
-	vim.api.nvim_create_autocmd('WinLeave', {
-		buffer = buf,
-		callback = function()
-			vim.api.nvim_win_close(shower.win, true)
-		end
-	})
-
-	vim.keymap.set({ 'i', 'n' }, "<CR>", "<CR><Esc>:close!<CR>:stopinsert<CR>", { silent = true, buffer = buf })
-	vim.keymap.set('n', '<Esc>', "<cmd>close!<CR>", { silent = true, buffer = buf })
-
+	local pick_config = H.get_window_config({ title = "picker" })
+	local picker = H.create_floating_window(pick_config, false)
+	vim.wo[picker.win].cul = true
+	---@type vim.api.keyset.win_config
 	local default_win_options = {
-		relative = "editor",
-		row = vim.o.lines / 2 - 1,
-		col = vim.o.columns / 2 - 25,
-		width = 50,
+		relative = "win",
+		win = picker.win,
+		row = -4,
+		col = -1,
 		height = 1,
 		focusable = true,
 		style = "minimal",
@@ -106,8 +63,78 @@ Nux.workspace_adder = function(on_confirm)
 		title = " Select a workspace root "
 	}
 
+	local workspace_comp_config = H.get_window_config({
+		relative = "win",
+		win = picker.win,
+		height = math.floor(.68 * .68 * vim.o.lines) + 2,
+		row = -4,
+		col = vim.api.nvim_win_get_config(picker.win).width + 1,
+	})
+
+	local workspace_comp = H.create_floating_window(workspace_comp_config, false)
+
+	local prompt = H.create_floating_window(H.get_window_config(default_win_options), true)
+	vim.bo[prompt.buf].buftype = 'prompt'
+	-- TODO : completion function for folders
+	vim.bo[prompt.buf].bufhidden = 'wipe'
+
+	local defered_callback = function(input)
+		vim.defer_fn(function()
+			on_confirm(input)
+		end, 10)
+	end
+
+	vim.fn.prompt_setprompt(prompt.buf, '')
+	vim.fn.prompt_setcallback(prompt.buf, defered_callback)
+
+	vim.keymap.set('i', Nux.config.key_mappings.forward, function()
+		local cursor = vim.api.nvim_win_get_cursor(picker.win)
+		vim.api.nvim_win_set_cursor(picker.win, { math.max(cursor[1] + 1), 0 })
+	end, { buffer = prompt.buf }
+	)
+
+	vim.keymap.set('i', Nux.config.key_mappings.backward, function()
+		local cursor = vim.api.nvim_win_get_cursor(picker.win)
+		vim.api.nvim_win_set_cursor(picker.win, { math.max(cursor[1] - 1, 1), 0 })
+	end, { buffer = prompt.buf }
+	)
+
+	vim.api.nvim_create_autocmd('TextChangedI', {
+		buffer = prompt.buf,
+		callback = function()
+			-- vim.print(vim.fn.prompt_getinput(buf))
+			local ns = vim.api.nvim_create_namespace("MyHighlightNs")
+			local user_path = H.parse_path(vim.fn.prompt_getinput(prompt.buf))
+			local files = vim.fn.readdir(user_path)
+			vim.api.nvim_buf_set_lines(picker.buf, 0, -1, false, files)
+			for i, v in pairs(files) do
+				local stat = vim.uv.fs_stat(user_path .. '/' .. v)
+				-- vim.api.nvim_buf_set_lines(shower.buf, i - 1, i - 1, false, { v })
+				local hl = (stat and stat.type == "directory") and "Directory" or "Normal"
+				vim.api.nvim_buf_set_extmark(picker.buf, ns, i - 1, 0, {
+					end_line = i,
+					hl_group = hl,
+				})
+			end
+			-- vim.api.nvim_buf_set_lines(shower.buf, 0, -1, false, files)
+			H.update_win(picker.win, { title_pos = "right", title = tostring(#files) })
+		end
+	})
+
+	vim.api.nvim_create_autocmd('WinLeave', {
+		buffer = prompt.buf,
+		callback = function()
+			vim.api.nvim_win_close(picker.win, true)
+			vim.api.nvim_win_close(workspace_comp.win, true)
+		end
+	})
+
+	vim.keymap.set({ 'i', 'n' }, "<CR>", "<CR><Esc>:close!<CR>:stopinsert<CR>", { silent = true, buffer = prompt.buf })
+	vim.keymap.set('n', '<Esc>', "<cmd>close!<CR>", { silent = true, buffer = prompt.buf })
+
+
 	-- win_opts = vim.tbl_deep_extend)
-	local win = vim.api.nvim_open_win(buf, true, default_win_options)
+	-- local win = vim.api.nvim_open_win(prompt.buf, true, default_win_options)
 	vim.cmd("startinsert")
 
 	-- vim.defer_fn(function ()
@@ -118,10 +145,15 @@ end
 
 
 
+--- root { files } { {specials, opt} }
 Nux.add_workspace = function(root, ...)
 	H.check_filereadable()
 	local parsed_path = H.parse_path(H.get_config().workspace.file_path)
 	vim.fn.writefile({ root }, parsed_path, 'a')
+end
+
+-- TODO rename function `inplace` boolean in not then just a :t var
+Nux.rename = function(workspace, inplace)
 end
 
 -- Helper --------------------------------------------------------------
@@ -134,6 +166,12 @@ H.setup_hl = function()
 	hl('NuxCursor', { blend = 100, nocombine = true })
 end
 
+-- TODO to update maybe
+H.update_win = function(win, opts)
+	local current = vim.api.nvim_win_get_config(win)
+	local updated_config = vim.tbl_deep_extend('force', current, opts)
+	vim.api.nvim_win_set_config(win, updated_config)
+end
 -- TODO
 -- --- Will just return the height / width, col, line of each windows
 -- H.layouts.centric = function(cols, rows)
@@ -150,8 +188,8 @@ H.check_config = function(config)
 end
 
 H.check_filereadable = function()
-	local parsed_path = H.parse_path(H.get_config().workspace.file_path)
-	return vim.fn.filereadable(parsed_path) == 1
+	local path = H.get_path()
+	return vim.fn.filereadable(path) == 1
 end
 
 -- TODO
@@ -186,14 +224,21 @@ H.parse_path = function(path)
 	return vim.fs.normalize(path)
 end
 
-
-H.load_projects = function(path)
+H.get_path = function()
+	local path = H.get_config().workspace.file_path
 	local parsed_path = H.parse_path(path)
-	if vim.fn.filereadable(path) == 0 then return {} end
-	local projects = vim.fn.readfile(parsed_path)
-	if vim.tbl_isempty(projects) then return {} end
-	return vim.fn.json_decode(table.concat(projects, "\n"))
+	return parsed_path
 end
+
+H.load_projects = function()
+	local path = H.get_path()
+	if vim.fn.filereadable(path) == 0 then return {} end
+	local projects = vim.fn.readfile(path)
+	if vim.tbl_isempty(projects) then return {} end
+	return vim.fn.json_decode(projects)
+	-- return vim.fn.json_decode(table.concat(projects, "\n"))
+end
+
 
 -- TO REWORK
 H.open_project = function(project, mode)
@@ -256,7 +301,7 @@ H.default_config = vim.deepcopy(Nux.config)
 -- 	end)
 
 Nux.setup()
-
+--
 -- H.cache.guicursor = vim.o.guicursor
 -- local side_bar_float = H.create_floating_window(H.get_window_config({ title = " Pick a project " }))
 -- local main_content_float = H.create_floating_window(H.get_window_config(function()
@@ -274,6 +319,7 @@ Nux.setup()
 -- local titles = {}
 -- local project_keys = {}
 -- local projects = H.load_projects(H.get_config().workspace.file_path)
+-- vim.print(projects)
 -- for k, v in pairs(projects) do
 -- 	local centered_title = k .. string.rep(" ", vim.api.nvim_win_get_width(side_bar_float.win) - #k - 3) .. "[L]"
 -- 	table.insert(titles, centered_title)
@@ -324,4 +370,34 @@ Nux.setup()
 -- 	H.open_project(projects[current_project_key])
 -- end, { buffer = side_bar_float.buf })
 --
-Nux.workspace_adder(function() vim.print("confirmed") end)
+-- Nux.workspace_adder(function() vim.print("confirmed") end)
+local tests = {
+	{
+		root = "~/.config/nvim",
+		files = { "init.lua" }
+	},
+	{
+		root = "~/Documents/AREAS/riskpat/patrisk-amplify",
+		files = { "backend.ts" }
+	}
+}
+
+local write_json = function(path, tbl)
+	local json_str = vim.fn.json_encode(tbl)
+	local file = assert(io.open(path, "w"))
+	file:write(json_str)
+	file:close()
+end
+
+local read_json = function(path)
+	local file = assert(io.open(path, "r"))
+	local content = file:read("*a")
+	file:close()
+	return vim.fn.json_decode(content)
+end
+
+
+local parsed = H.parse_path("~/.nux_workspaces")
+-- write_json(parsed, tests)
+-- vim.print(read_json(parsed))
+vim.print(vim.fn.filereadable(parsed))
